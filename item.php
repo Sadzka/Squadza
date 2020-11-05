f486f791f616a3f2dbcb9360e87f4955a02bb154
Sadzka <sirsadzka@gmail.com>
2020-11-05 19:26:10 +0100

    item display prototype

---
+++ item.php
@@ -0,0 +1,65 @@
+<html lang="en">
+
+<head>
+	<title>Arikazike</title>
+	<link rel="stylesheet" href="css/items.css">
+</head>
+
+<body>
+
+	<div class="itemtooltip">
+		<p class="iconlarge">
+			<img src="img/icons/itemborder.png" />
+			<img class="itemicon" src="img/icons/inv_axe_113.jpg" />
+		</p>
+				
+		<table class="tooltip">
+		<th>
+			
+			<div class="item">
+				<span class="q5 name">Sadzmourne</span> <br>
+				
+				<span class="ilvl">Item Level <span id="ilvlvalue">272</span></span> <br>
+				<span id="boe">Binds when pick up</span> <br>
+				<span id="Unique">Unique</span> <br>
+				<span id="eqtype">Two-Hand</span> <span id="ittype" class="alignright">Sword</span> <br>
+				<span id="damage">515 - 648 Damage</span> <span id="speed" class="alignright">Speed 3.6</span> <br>
+				<span id="dps">(161.5 Damage per second)</span> <br>
+				<span> +70 Strength </span> <br>
+				<span> +70 Stamina </span> <br>
+				<span class="q2"> +70 Crit </span> <br>
+				<span class="q2"> +70 Haste </span> <br>
+				
+				<div class="sockets">
+					<p class="socket q1 s-red"></p>
+					<p class="socket q1 s-green"></p>
+					<p class="socket q1 s-blue"></p>
+					<p class="socket q1 s-gray"></p>
+					<span class="q1"> Socket Bonus: +10 Strength </span>
+				</div>
+				
+				
+				<span>Required Level 50</span> <br>
+				
+				<div class="moneys">
+					Sell Price:
+					13 <p class="money m-gold" />
+					20 <p class="money m-silver" />
+					87 <p class="money m-copper" />
+				</div>
+				
+				<th class="top-right" />
+				
+				<tr>
+					<th class="bottom-left"></th>
+					<th class="bottom-right"></th>
+				</tr>
+				
+				<!-- <th style="background-position: top right;"></th> -->
+				<!-- <tr><th style="background-position: bottom left"></th><th style="background-position: bottom right"></th></tr>  -->
+				
+			</div>
+		</th>
+	</table>
+	</div>
+</body>
